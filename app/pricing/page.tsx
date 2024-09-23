7f26f742bb985708db3423131e9e001208ef72a7
Alfred <ealfred828@gmail.com>
2024-09-23 19:25:13 +0100

    fixed deployment errors

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -10,7 +10,7 @@ function page() {
         <div className='h-[70vh] flex flex-col items-center justify-center'>
             <h1 className='text-2xl font-medium'>No plans available</h1>
             <p className='text-lg mt-5 text-balance w-[70%] md:w-auto'>
-                Once there are plans available for purchase, you'll see them here.
+                Once there are plans available for purchase, you&apos;ll see them here.
             </p>
             <Link href={'/'} className='px-3 py-2 text-black bg-primary mt-8'>Back to Homepage</Link>
         </div>
