27872c2e44b56a06dc6fec45c2624b711b8e5efb
Alfred <ealfred828@gmail.com>
2024-09-23 15:08:57 +0100

    finished working on the blog page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,9 +11,11 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#effe8a",
       },
     },
   },
   plugins: [],
 };
+
 export default config;
